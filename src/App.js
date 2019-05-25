a053ef595c1c8d09ed64f5e5da8f6156a0ca1aac
sieunlim225 <sieunlim225@gmail.com>
2019-05-26 00:17:52 +0900

    bootstrap testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Main from './main';
+import Header from './header';
+import TableField from './table';
+
 class App extends Component {
   render(){
     return (
+      
       <div className="App">
+        <Header/>
+        <h1>안녕하세요! 시은시은</h1>
         <Main/>
+        <TableField/>
       </div>
     )
   }
