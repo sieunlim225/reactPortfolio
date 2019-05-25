896d36f70f30fb57e06cbfba65b95c2d72b58745
sieunlim225 <sieunlim225@gmail.com>
2019-05-25 23:19:48 +0900

    testing

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './main.css';
+
+class Main extends Component{
+    render(){
+        return(
+            <div>testing</div>
+        )
+    }
+}
+
+export default Main
